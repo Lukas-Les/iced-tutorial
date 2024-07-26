cc386a11dd078e636c51dec585c362b2c5ff57a6
stabmeldys <lukas.les93@gmail.com>
2024-07-26 19:12:35 +0300

    text_input instead of text_editor

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use iced::{Sandbox, Command, Element, Settings, Theme, Renderer};
-use iced::widget::{Button, row, Text};
-
+use iced::widget::text_input::{self, Value};
+use iced::{alignment, Alignment, Command, Element, Renderer, Sandbox, Settings, Theme};
+use iced::widget::{text, text_editor, Button, Column, Container, Row, TextInput};
 
 pub fn main() -> iced::Result {
     Hello::run(Settings::default())
 }
 
 struct Hello {
-    target_files: Vec<Option<String>>,
-    result: Option<String>,
+    target_paths: Option<Vec<String>>,
+    content: text_input::Value,
 }
 
 impl Sandbox for Hello {
@@ -16,8 +16,8 @@ impl Sandbox for Hello {
 
     fn new() -> Hello {
         Hello {
-            target_files: Vec::new(),
-            result: None
+            target_paths: None,
+            content: text_input::Value::new("The results will be desplayed here"),
         }
     }
 
@@ -27,30 +27,44 @@ impl Sandbox for Hello {
 
     fn update(&mut self, message: Self::Message) {
         match message {
-            Message::LEFT_PRESSED => {
-                self.result = Some("pressed".to_string());
+            Message::SelectFiles => {
+                self.target_paths = Some(vec!["a".to_string(), "b".to_string(), "new".to_string()]);
+                self.content = text_input::Value::new("files");
             }
-            Message::RIGHT_PRESSED => {
-                self.result = None;
+            Message::SelectDir => {
+                self.target_paths = Some(vec!["d".to_string()]);
+                self.content = text_input::Value::new("dir");
+            }
+            Message::ResetResult => {
+                self.target_paths = None;
+                self.content = text_input::Value::new("The results will be desplayed here");
             }
         }
     }
 
     fn view(&self) -> Element<Self::Message> {
-        let left_button: iced::widget::Button<'_, Message, Theme, Renderer> = Button::new("LEFT").on_press(Message::LEFT_PRESSED);
-        let right_button: iced::widget::Button<'_, Message, Theme, Renderer> = Button::new("RIGHT").on_press(Message::RIGHT_PRESSED);
-        let result_text = self.result.clone().unwrap_or_default();
-        let content = row![
-            left_button, 
-            Text::new(result_text),
-            right_button,
-        ];
+        let top_row = Row::new()
+            .push(Button::new("Select Files").on_press(Message::SelectFiles))
+            .push(Button::new("Select directory").on_press(Message::SelectDir))
+            .push(Button::new("Reset").on_press(Message::ResetResult))
+            .spacing(3)
+            .align_items(alignment::Alignment::Center);
+
+        let top_row_container = Container::new(top_row)
+            .padding(3)
+            .center_x();
+
+        let result_display = TextInput::new("placeholder", &self.content.to_string());
+        let content = Column::new()
+            .push(top_row_container)
+            .push(result_display);
         content.into()
     }
 }
 
 #[derive(Clone, Debug)]
 enum Message {
-    LEFT_PRESSED,
-    RIGHT_PRESSED,
+    SelectFiles,
+    SelectDir,
+    ResetResult,
 }
